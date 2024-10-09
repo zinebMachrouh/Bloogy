9932e6d3ce36aa39fadf1331d86ab3aadb6010f4
Rayane Fiach <139344446+Rayane20777@users.noreply.github.com>
2024-10-09 13:50:41 +0100

    Category service done

--- src/main/java/services/Interfaces/CategoryService.java
+++ src/main/java/services/Interfaces/CategoryService.java
@@ -10,6 +10,6 @@ public interface CategoryService {
     public Category addCategory(CategoryDTO category) throws SQLException;
     public Category updateCategory(CategoryDTO category) throws SQLException;
     void deleteCategory(int id) throws SQLException;
-    List<Category> getAllCategories() throws SQLException;
+    List<CategoryDTO> getAllCategories() throws SQLException;
     Category getCategoryById(int id) throws SQLException;
 }

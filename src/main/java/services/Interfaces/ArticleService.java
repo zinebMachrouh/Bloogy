210d8a948911feaef6b9664861163c8d5f254eb0
Rayane Fiach <139344446+Rayane20777@users.noreply.github.com>
2024-10-09 11:27:03 +0100

    adding category service interface

--- src/main/java/services/Interfaces/ArticleService.java
+++ src/main/java/services/Interfaces/ArticleService.java
@@ -6,7 +6,7 @@ import models.Article;
 import java.sql.SQLException;
 import java.util.List;
 
-interface ArticleService {
+public interface ArticleService {
     public Article addArticle(ArticleDTO article) throws SQLException;
     public Article updateArticle(ArticleDTO article) throws SQLException;
     void deleteArticle(int id) throws SQLException;
